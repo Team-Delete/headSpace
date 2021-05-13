a1b69fbd74364084953f07eb79614724bcfb0e63
Anthony Williams <75553592+Williamsjanthony15@users.noreply.github.com>
2021-05-13 00:01:33 -0500

    Add files via upload

---
+++ HeadSpace/src/Jokes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// API CALL
+
+// STATIC NO DB required
+
+// 
+
+class Jokes extends React.Component {
+  render() {
+    return(
+      <>
+      <h2>Hello!</h2>
+      </>
+    )
+  }
+}
+export default Jokes;
