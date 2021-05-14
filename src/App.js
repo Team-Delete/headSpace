e73481983dbc0aa249679319e43e0476b3fea8fb
Adrian Milsap <adrian@hirobius.com>
2021-05-13 20:41:35 -0700

    dependency install

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import React from 'react';
 // import Profile from './Profile';
 // import Weather from './Weather';
 // import Quote from './Quote';
-// import Jokes from './Jokes';
-
-
+import Jokes from './Jokes';
 
 // Use for the main stream line component. Should have low functions, high imports for render.
 
 // Render compenent -- shouldn't need bootstrap.
-
-
 
 class App extends React.Component {
   render() {
     return(
       <>
       <h2>Hello!</h2>
+      <Jokes />
       </>
     )
   }
--- src/Jokes.js
+++ src/Jokes.js
@@ -1,16 +1,65 @@
 import React from 'react';
 
+const https = require("https"); // Native module, no need to explicitly install
+
+const baseURL = "https://v2.jokeapi.dev";
+const categories = ["Programming", "Misc", "Pun", "Spooky", "Christmas"];
+const params = [
+  "blacklistFlags=nsfw, religious, political, racist, sexist, explicit",
+  "idRange=0-100"
+];
+
+https.get(`${baseURL}/joke/${categories.join(",")}?${params.join("&")}`, res => {
+  console.log("\n");
+  res.on("data", chunk => {
+    // On data received, convert it to a JSON object
+    let randomJoke = JSON.parse(chunk.toString());
+
+    if (randomJoke.type === "single") {
+      // If type == "single", the joke only has the "joke" property
+      console.log(randomJoke.joke);
+      console.log("\n");
+    }
+    else {
+      // If type == "twopart", the joke has the "setup" and "delivery" properties
+      console.log(randomJoke.setup);
+      setTimeout(() => {
+        console.log(randomJoke.delivery);
+        console.log("\n");
+      }, 3000);
+    }
+  });
+
+  res.on("error", err => {
+    // On error, log to console
+    console.error(`Error: ${err}`);
+  });
+});
+
+
 // API CALL
+const JokeAPI = require('sv443-joke-api');
 
-// STATIC NO DB required
-
-// 
+JokeAPI.getJokes()
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
+  })
 
 class Jokes extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
-    return(
+    return (
       <>
-      <h2>Hello!</h2>
+        <h2>Jokes</h2>
+        <div id="joke-container">
+          <h2 id="joke--text"></h2>
+        </div>
       </>
     )
   }
