0018c44330d01cc1a958cdf1e3c68544af382882
Adrian Milsap <adrian@hirobius.com>
2021-05-15 10:58:17 -0700

    gitignore update and working JOKE API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
+import axios from 'axios';
 import React from 'react';
-// import Mood from './Mood';
-// import Profile from './Profile';
-// import Weather from './Weather';
-// import Quote from './Quote';
 import Jokes from './Jokes';
 
-// Use for the main stream line component. Should have low functions, high imports for render.
-
-// Render compenent -- shouldn't need bootstrap.
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  handleMoodSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      this.getJoke();
+    } catch (err) {
+      console.log(err);
+      this.setState({ error: `${err.message}: ${err.message.data}` });
+    }
+  }
+
+  getJoke = async () => {
+    try {
+      let jokeData = await axios.get(`https://v2.jokeapi.dev/joke/Dark?format=json&blacklistFlags=nsfw,sexist&type=single&lang=en&amount=1`);
+      // console.log(jokeData.data.joke);
+      this.setState({ jokeData: jokeData.data.joke});
+    } catch (err) {
+      console.log(`error found!!! ${err.message}`);
+      this.setState({ error: `${err.message}: ${err.message.data}` });
+    }
+  }
+
   render() {
-    return(
+    return (
       <>
-      <h2>Hello!</h2>
-      <Jokes />
+        <button onClick={this.handleMoodSubmit}>
+          Set Mood and call Joke API
+        </button>
+        {/* <h2>Returned Joke:</h2> */}
+        {/* <p>{jokeData}</p> */}
+        <Jokes jokeData={this.state.jokeData} />
       </>
     )
   }
 }
+
 export default App;
--- src/Jokes.js
+++ src/Jokes.js
@@ -1,54 +1,4 @@
 import React from 'react';
-import axios from 'axios';
-
-// const https = require('https'); // Native module, no need to explicitly install
-
-const baseURL = 'https://v2.jokeapi.dev';
-// const categories = ['Programming', 'Misc', 'Pun', 'Spooky', 'Christmas'];
-const categories = ['Programming'];
-const params = [
-  'blacklistFlags=nsfw, religious, political, racist, sexist, explicit',
-  'idRange=0-100'
-];
-
-// Get a Joke!!!!
-// https://v2.jokeapi.dev/joke/[Category/-ies]
-
-axios.get(`${baseURL}/joke/${categories}?${params.join('&')}`, res => {
-  // console.log('\n');
-  res.on('data', chunk => {
-    // On data received, convert it to a JSON object
-    let randomJoke = JSON.parse(chunk.toString());
-
-    if (randomJoke.type === 'single') {
-      // If type == 'single', the joke only has the 'joke' property
-      console.log(randomJoke.joke);
-      // console.log('\n');
-    }
-    else {
-      // If type == 'twopart', the joke has the 'setup' and 'delivery' properties
-      console.log(randomJoke.setup);
-      setTimeout(() => {
-        console.log(randomJoke.delivery);
-        console.log('\n');
-      }, 3000);
-    }
-  });
-
-  res.on('error', err => {
-    // On error, log to console
-    console.error(`Error: ${err}`);
-  });
-});
-
-// API CALL
-// const JokeAPI = require('sv443-joke-api');
-
-// JokeAPI.getJokes()
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data)
-//   })
 
 class Jokes extends React.Component {
   constructor(props) {
@@ -60,10 +10,10 @@ class Jokes extends React.Component {
   render() {
     return (
       <>
-        <h2>Jokes</h2>
         <div id='joke-container'>
-          <h2 id='joke--text'>Joke Text</h2>
+          <h2 id='joke--text'>{this.props.jokeData}</h2>
         </div>
+
       </>
     );
   }
