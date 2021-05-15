fea4cc7580e3ae4ac37e912cde3f2dfb017c6c6f
Kevin Henry <kevinhenry@mac.com>
2021-05-15 14:01:45 -0700

    Random Quotes and Next Button Working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Quote from './Quote';
+import './App.css';
 // import Mood from './Mood';
 // import Profile from './Profile';
 // import Weather from './Weather';
-// import Quote from './Quote';
 // import Jokes from './Jokes';
-
-
 
 // Use for the main stream line component. Should have low functions, high imports for render.
 
-// Render compenent -- shouldn't need bootstrap.
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    console.log('constructor runs');
+  }
 
+  componentDidMount() {
+    console.log('componentDidMount runs');
+  }
 
-class App extends React.Component {
+  // Render compenent -- shouldn't need bootstrap.
+
   render() {
-    return(
-      <>
-      <h2>Hello!</h2>
-      </>
-    )
+    console.log('render method runs');
+    return (
+      <div>
+        {/* <h1>Hello</h1> */}
+        <Quote/>
+      </div>
+    );
   }
 }
+
 export default App;
--- src/Jokes.js
+++ src/Jokes.js
@@ -4,15 +4,15 @@ import React from 'react';
 
 // STATIC NO DB required
 
-// 
+// Told me to
 
 class Jokes extends React.Component {
   render() {
     return(
       <>
-      <h2>Hello!</h2>
+        <h2>Hello!</h2>
       </>
-    )
+    );
   }
 }
 export default Jokes;
